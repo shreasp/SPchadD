0f05e66db696dd27345a0e9fb4dacbc0db373543
shreas <shreaspatil999@gmail.com>
2023-07-02 16:55:31 +0530

    reg

--- new@2/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/ud/springboot/demo/myapp/rest/FunRestController.java
+++ new@2/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/ud/springboot/demo/myapp/rest/FunRestController.java
@@ -8,4 +8,15 @@ public class FunRestController {
     public String hello(){
         return "Hello World!";
     }
+
+    // expose new endpoint for workout
+    @GetMapping ("/workout")
+    public String getDailyWorkout(){
+       return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+       return "Lucky Day";
+    }
 }
