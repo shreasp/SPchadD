7329271a1085c5302f423912f1309f23c2c7f2cc
shreas <shreaspatil999@gmail.com>
2023-07-03 15:30:40 +0530

    reglr

--- new@2/01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/ud/springboot/demo/myapp/MyappApplication.java
+++ new@2/01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/ud/springboot/demo/myapp/MyappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyappApplication {
 
-	public static void main(String[] args) {
+	public static  void main(String[] args) {
 
 		SpringApplication.run(MyappApplication.class, args);
 	}
