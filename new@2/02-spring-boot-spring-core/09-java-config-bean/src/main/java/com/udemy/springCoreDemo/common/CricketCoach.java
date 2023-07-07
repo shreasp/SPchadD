bd305476aa4fc6aaef6d670814df115b0f409c55
shreas <shreaspatil999@gmail.com>
2023-07-07 17:02:40 +0530

    sap

---
+++ new@2/02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/udemy/springCoreDemo/common/CricketCoach.java
@@ -0,0 +1,20 @@
+package com.udemy.springCoreDemo.common;
+
+import com.udemy.springCoreDemo.common.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In Constructor" + getClass().getSimpleName());
+    }
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice Bowling for 15 mins";
+    }
+}
