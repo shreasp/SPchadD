86e5072413f4baaacbf5776118a3d88a8685d841
shreas <shreaspatil999@gmail.com>
2023-07-10 23:37:31 +0530

    reglr

--- new@2/02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/udemy/springCoreDemo/common/CricketCoach.java
+++ new@2/02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/udemy/springCoreDemo/common/CricketCoach.java
@@ -15,6 +15,6 @@ public class CricketCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Practice Bowling for 15 mins ";
+        return "Practices Bowling for 15 mins ";
     }
 }
