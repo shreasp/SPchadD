145a7e5c514429a68f97315cbb352d0dedc90ac6
shreas <shreaspatil999@gmail.com>
2023-08-06 00:00:28 +0530

    reglr

--- new@2/04-spring-boot-REST-crud/01-spring-boot-REST-crud/src/main/java/com/udemy/dem/rest/DemoRestController.java
+++ new@2/04-spring-boot-REST-crud/01-spring-boot-REST-crud/src/main/java/com/udemy/dem/rest/DemoRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class DemoRestController {
 
-    // add code for the /hello end point
+    // add code for the /hello endpoint
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello World!";
