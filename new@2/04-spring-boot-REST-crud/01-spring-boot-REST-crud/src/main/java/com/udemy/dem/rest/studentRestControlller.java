5e637897f3088189e63d27b72c7dd933e04db1e3
shreas <shreaspatil999@gmail.com>
2023-08-07 23:55:29 +0530

    reglr

---
+++ new@2/04-spring-boot-REST-crud/01-spring-boot-REST-crud/src/main/java/com/udemy/dem/rest/studentRestControlller.java
@@ -0,0 +1,31 @@
+package com.udemy.dem.rest;
+
+import com.udemy.dem.Entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class studentRestControlller {
+
+    // define endpoint for /students to return list opf students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("poorna","patel"));
+        theStudents.add(new Student("mario","rossi"));
+        theStudents.add(new Student("mary","smith"));
+
+        return theStudents;
+    }
+
+
+
+
+}
