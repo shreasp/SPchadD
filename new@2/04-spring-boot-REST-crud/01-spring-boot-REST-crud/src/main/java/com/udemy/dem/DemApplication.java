fb50b2c6b0eaf90bc88cc0777a04dca7866a61d1
shreas <shreaspatil999@gmail.com>
2023-08-08 15:33:08 +0530

    reglr

--- new@2/04-spring-boot-REST-crud/01-spring-boot-REST-crud/src/main/java/com/udemy/dem/DemApplication.java
+++ new@2/04-spring-boot-REST-crud/01-spring-boot-REST-crud/src/main/java/com/udemy/dem/DemApplication.java
@@ -10,4 +10,6 @@ public class DemApplication {
 		SpringApplication.run(DemApplication.class, args);
 	}
 
+
+
 }
--- new@2/04-spring-boot-REST-crud/01-spring-boot-REST-crud/src/main/java/com/udemy/dem/rest/studentRestControlller.java
+++ new@2/04-spring-boot-REST-crud/01-spring-boot-REST-crud/src/main/java/com/udemy/dem/rest/studentRestControlller.java
@@ -1,7 +1,10 @@
 package com.udemy.dem.rest;
 
 import com.udemy.dem.Entity.Student;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,20 +15,38 @@ import java.util.List;
 @RequestMapping("/api")
 public class studentRestControlller {
 
-    // define endpoint for /students to return list opf students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    private List < Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstruct to loan students data...only once!!
+    @PostConstruct
+    public void loadData(){
+
+        theStudents= new ArrayList<>();
 
         theStudents.add(new Student("poorna","patel"));
         theStudents.add(new Student("mario","rossi"));
         theStudents.add(new Student("mary","smith"));
 
+    }
+
+    // define endpoint for /students to return list opf students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+
+
         return theStudents;
     }
 
+    // define endpoint or "/students/{studentId}" to return student at index
 
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // just index into the list
+
+        return theStudents.get(studentId);
+    }
 
 
 }
