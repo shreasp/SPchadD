3936b31f1e56b110212e8ebd04b9273b2c6642c4
shreas <shreaspatil999@gmail.com>
2023-08-12 15:36:12 +0530

    reglr

--- new@2/04-spring-boot-REST-crud/1.6-spring-boot-REST-crud - w-excep/src/main/java/com/udemy/dem/rest/studentRestControlller.java
+++ new@2/04-spring-boot-REST-crud/1.6-spring-boot-REST-crud - w-excep/src/main/java/com/udemy/dem/rest/studentRestControlller.java
@@ -70,5 +70,25 @@ public class studentRestControlller {
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 
+    // add another exception handler ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+        StudentErrorResponse error =new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+
+        // Return response entity
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+
+
+
+
+    }
+
+
 
 }
