41ee3232be73ae7e463fc380dbd12dbf332a437a
shreas <shreaspatil999@gmail.com>
2023-08-18 19:45:37 +0530

    reglr

---
+++ new@2/04-spring-boot-REST-crud/3-spring-boot-rest-crud-employee-proj - DTA-JPA/src/main/java/com/udemy/springboot/cruddemo/Rest/employeeRestController.java
@@ -0,0 +1,69 @@
+package com.udemy.springboot.cruddemo.Rest;
+
+
+import com.udemy.springboot.cruddemo.Entity.Employee;
+import com.udemy.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class employeeRestController {
+
+    private EmployeeService employeeService;
+
+
+    @Autowired
+    public employeeRestController(EmployeeService theEmployeeService){
+        employeeService= theEmployeeService;
+    }
+    // expose /employees and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+
+    }
+
+    // add mapping for GET employee w id
+    @GetMapping("/employees/{employeeId}")
+    public Employee getById(@PathVariable int employeeId){
+        Employee theEmployee= employeeService.findById(employeeId);
+        if (theEmployee== null){
+            throw new RuntimeException("Employee id not found -" + employeeId);
+        }
+        return theEmployee;
+    }
+
+    // add mapping for POST employees  - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        // also just in case if they pass an id in json set id =0
+        // that is to save new item instead of an update
+
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    //add PUT mapping to update employees
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee= employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId ){
+        Employee  tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (tempEmployee == null ){
+            throw new RuntimeException("Employee Id not found - "+ employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee Id - "+ employeeId;
+    }
+}
---
+++ new@2/04-spring-boot-REST-crud/3-spring-boot-rest-crud-employee-proj - DTA-JPA/src/main/java/com/udemy/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.udemy.springboot.cruddemo.service;
+
+import com.udemy.springboot.cruddemo.Entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List <Employee>  findAll();
+
+    Employee findById(int theId);
+
+    Employee save (Employee theEmployee);
+
+    Employee deleteById(int theId);
+}
