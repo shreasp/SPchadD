4a7d070b39a808e79ddc5b502130668e6d5f4286
shreas <shreaspatil999@gmail.com>
2023-08-19 14:10:27 +0530

    reglr

--- new@2/04-spring-boot-REST-crud/4-spring-boot-rest-crud-employee-proj - RSTDTA/src/main/java/com/udemy/springboot/cruddemo/DAO/EmployeeRepository.java
+++ new@2/04-spring-boot-REST-crud/4-spring-boot-rest-crud-employee-proj - RSTDTA/src/main/java/com/udemy/springboot/cruddemo/DAO/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.udemy.springboot.cruddemo.DAO;
 
 import com.udemy.springboot.cruddemo.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
